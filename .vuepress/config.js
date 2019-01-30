ab029d4485ada531084b71741657b33a45941fe2
Aleksey Razbakov <aleksey@razbakov.com>
2019-01-30 10:38:50 +0100

    Add edit page link

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,10 +2,12 @@ module.exports = {
   title: 'Vida alemana',
   description: 'Información útil sobre la vida en Alemania',
   themeConfig: {
+    repo: 'razbakov/faq-es',
     sidebarDepth: 2,
+    editLinks: true,
+    editLinkText: 'Edita esta página',
     nav: [
       { text: 'Inicio', link: '/' },
-      { text: 'GitHub', link: 'https://github.com/razbakov/faq-es' },
     ],
     sidebar: [
       '/'
